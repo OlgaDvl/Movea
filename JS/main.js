cfcb48ac60b0c351e47c96710acd7b325bf33f5f
OlgaDvl <sov1308@rambler.ru>
2022-04-11 12:42:39 +0300

    Устранена проблема с видео
    Видео не проигрывается после закрытия модального окна

--- JS/main.js
+++ JS/main.js
@@ -3,10 +3,12 @@ const buttonsModal = document.querySelectorAll('.button-play');
 
 buttonsModal.forEach((item, i) => {
     item.addEventListener('click', () => {
+        document.getElementById('vid').src = 'https://www.youtube.com/embed/razrg0M36Xc';
         modalWindow.classList.add('active');
     })
 });
 
 modalWindow.addEventListener('click', () => {
     modalWindow.classList.remove('active');
+    document.getElementById('vid').src = '';
 })
